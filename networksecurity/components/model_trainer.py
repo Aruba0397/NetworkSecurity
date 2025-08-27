c2f0e383c24ed12dda39eeeb99309bbc043b64bb
Aruba <aruba0397@gmail.com>
2025-08-27 15:53:10 +0500

    mlflow

--- networksecurity/components/model_trainer.py
+++ networksecurity/components/model_trainer.py
@@ -15,6 +15,7 @@ from networksecurity.utils.ml_utils.metric.classification_metric import get_clas
 
 import sys
 import os
+import mlflow
 
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import r2_score
@@ -33,7 +34,19 @@ class ModelTrainer:
             self.data_transformation_artifact=data_transformation_artifact
         except Exception as e:
             raise CustomException(e,sys)
-        
+      
+      # track mlflow
+    def  track_mlflow(self, best_model, classificationmetric):
+        with mlflow.start_run():
+            f1_score=classificationmetric.f1_score
+            precision_score=classificationmetric.precision_score
+            recall_score=classificationmetric.recall_score
+            
+            mlflow.log_metric("f1_score", f1_score)
+            mlflow.log_metric("precision", precision_score)
+            mlflow.log_metric("recall", recall_score)
+            mlflow.sklearn.log_model(best_model, "model")
+    
         #model evaluation
     def train_model(self,X_train,y_train,X_test,y_test):  
         models = {
@@ -86,8 +99,15 @@ class ModelTrainer:
         y_train_pred=best_model.predict(X_train)
         classification_train_metric=get_classification_score(y_true=y_train, y_pred=y_train_pred)
         
+        #tracking experiments with the mlflow
+        self.track_mlflow(best_model, classification_train_metric )
+        
         y_test_pred=best_model.predict(X_test)
         classification_test_metric=get_classification_score(y_true=y_test, y_pred=y_test_pred)
+        
+        
+        #tracking experiments with the mlflow
+        self.track_mlflow(best_model, classification_test_metric )
         
         #loading pickle file
         preprocessor = load_object(file_path=self.data_transformation_artifact.transformed_object_file_path)
--- networksecurity/utils/ml_utils/metric/classification_metric.py
+++ networksecurity/utils/ml_utils/metric/classification_metric.py
@@ -6,8 +6,9 @@ def get_classification_score(y_true,y_pred)->ClassificationMetricArtifact:
     try:
             
         model_f1_score = f1_score(y_true, y_pred)
-        model_recall_score = recall_score(y_true, y_pred)
         model_precision_score=precision_score(y_true,y_pred)
+        model_recall_score = recall_score(y_true, y_pred)
+    
 
         classification_metric =  ClassificationMetricArtifact(f1_score=model_f1_score,
                     precision_score=model_precision_score, 
